eca32243911ccc50e1b5148c33fb0da5f8898971
jimmyriguelle <jimmyriguelle@hotmail.com>
2021-11-19 08:26:23 +0100

    Initial commit

---
+++ android/app/src/main/java/be/riguelle/tik/MainActivity.java
@@ -0,0 +1,6 @@
+package be.riguelle.tik;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/user_constants.dart
@@ -0,0 +1,1 @@
+import 'list_of_imports.dart';
